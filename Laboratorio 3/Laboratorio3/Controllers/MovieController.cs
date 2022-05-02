d127684567cbbfa358f29e0a70ccc6432c6bdefb
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-05-01 19:12:49 -0600

    Added Lab3 files in a new folder

---
+++ Laboratorio 3/Laboratorio3/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movies = GetListOfMovies();
+            ViewBag.MainTitle = "List of my favorite films";
+            return View(movies);
+        }
+
+        private List<MovieModel> GetListOfMovies()
+        {
+            List<MovieModel> movies = new List<MovieModel>();
+            movies.Add(new MovieModel
+            {
+                Id = 1,
+                Name = "The Lord of the Rings: The Fellowship of the Ring",
+                Genre = "Epic fantasy/Adventure",
+                ReleasedDate = new DateTime(2001, 12, 10)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 2,
+                Name = "The Gentlemen",
+                Genre = "Action/Comedy",
+                ReleasedDate = new DateTime(2019, 12, 03)
+            });
+            movies.Add(new MovieModel
+            {
+                Id = 3,
+                Name = "Stonehearst Asylum",
+                Genre = "Gothic",
+                ReleasedDate = new DateTime(2014, 10, 24)
+            });
+
+            return movies;
+        }
+    }
+}
---
+++ Laboratorio 3/Laboratorio3/Controllers/SongController.cs
@@ -0,0 +1,42 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var song = GetSong();
+            ViewBag.MainTitle = "My favorite song";
+            ViewBag.Name = "Sovereign Light Café";
+            ViewBag.Band = "Keane";
+            ViewBag.Album = "Strangeland";
+            ViewBag.Genre = "Alternative rock/Pop rock";
+            ViewBag.ReleasedDate = new DateTime(2012, 07, 23);
+            //return View(song);
+            return View();
+        }
+
+        /**
+        private List<SongModel> GetSong()
+        {
+            List<SongModel> song = new List<SongModel>();
+            song.Add( new SongModel
+            {
+                Name = "Sovereign Light Café",
+                Band = "Keane",
+                Album = "Strangeland",
+                Genre = "Alternative rock/Pop rock",
+                ReleasedDate = new DateTime(2012, 07, 23)
+            });
+            
+            return song;
+        }
+        */
+    }
+}
