b72f972ea14dceb95a0078441e95f6efb0d0fc8c
Josef Ruzicka <63564928+JosefRuzicka@users.noreply.github.com>
2022-05-18 09:28:06 -0600

    Add files via upload

---
+++ Laboratorio 5/Controllers/PaisesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laboratorio_5.Handlers;
+
+namespace Laboratorio_5.Controllers
+{
+    public class PaisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            PaisesHandler paisesHandler = new PaisesHandler();
+            var paises = paisesHandler.ObtenerPaises();
+            ViewBag.MainTitle = "Lista de paises";
+            return View(paises);
+        }
+    }
+}
