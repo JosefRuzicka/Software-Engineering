22188874f20c7bf3232bc5120d4f6cf7a5ce8e60
Josef Ruzicka <63564928+JosefRuzicka@users.noreply.github.com>
2022-05-18 09:29:45 -0600

    Add files via upload

---
+++ Laboratorio 5/Models/PaisModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio_5.Models
+{
+    public class PaisModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
+    }
+}
