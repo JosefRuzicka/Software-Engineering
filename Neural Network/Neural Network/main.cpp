46231b92dcfaf11ac138f122cc8d59eb3d0e8322
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-06-03 20:09:31 -0600

    Nuevo Git

---
+++ Neural Network/Neural Network/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include "NeuralNetwork.hpp"
+
+
+using namespace std;
+
+int main()
+{
+    vector<vector<double> > values{
+            vector<double>{0,0,0},
+            vector<double>{1,0,0},
+            vector<double>{1,1,0},
+            vector<double>{1,1,1},
+            vector<double>{0,1,0},
+            vector<double>{0,1,1},
+            vector<double>{0,0,1},
+            vector<double>{1,0,1},
+
+    };
+
+    vector<vector<double>> expectedValues{
+            vector<double>{0},
+            vector<double>{1},
+            vector<double>{0},
+            vector<double>{1},
+            vector<double>{1},
+            vector<double>{0},
+            vector<double>{1},
+            vector<double>{0},
+
+
+
+
+    };
+
+
+
+    NeuralNetwork* neuralNetwork = new NeuralNetwork(/*3,100,1*/);
+
+    //neuralNetwork->trainNuevo(values[0], expectedValues[0]);
+    vector<double> output;
+    //std::cout << "de vuelta en main";
+    //
+        neuralNetwork->fit(values, expectedValues, 50000);
+    //
+    //
+    //
+        vector<vector<double>> input {
+                vector<double>{0,0,0}, {0,1,0}, {1,0,0}, {1,1,1}
+        };
+    //
+        for(auto value : values)
+        {
+           output.push_back( neuralNetwork->predict(value)[0]);
+    //
+        }
+        for (auto i:output) {
+            std::cout << i << endl;
+            if (i>0.5){
+                cout<< 1<<endl;
+            }
+            else{
+                cout<< 0<< endl;
+            }
+        }
+    //
+
+}
