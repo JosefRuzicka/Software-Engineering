275cd7e4c0441ff8ea3962c70636b3520e52cee7
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-23 14:26:27 -0600

    Se muestra cantidad de bebidas disponibles

--- Examen Final/vending-machine/src/components/OfferedBeveragesComp/OfferedBeveragesComp.js
+++ Examen Final/vending-machine/src/components/OfferedBeveragesComp/OfferedBeveragesComp.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './OfferedBeveragesCompStyle.scss';
 
-export const OfferedBeveragesComp = () => {
+export const OfferedBeveragesComp = ({beveragesOnStock}) => {
   return (
     <div className='parent'>
-      <div className='div1'> Coca Cola</div>
-      <div className='div2'> Pepsi </div>
-      <div className='div3'> Fanta </div>
-      <div className='div4'> Sprite </div>
+      <div className='div1'> 
+        <p> Coca Cola </p>
+        <p> On stock: {beveragesOnStock[0]} </p>
+      </div>
+      <div className='div2'> 
+        <p> Pepsi </p>
+        <p> On stock: {beveragesOnStock[1]} </p>
+      </div>
+      <div className='div3'> 
+        <p> Fanta </p>
+        <p> On stock: {beveragesOnStock[2]} </p>
+      </div>
+      <div className='div4'> 
+        <p> Sprite </p>
+        <p> On stock: {beveragesOnStock[3]} </p>
+      </div>
     </div>
   );
 };
--- Examen Final/vending-machine/src/components/VendingMachineLayoutComp/VendingMachineLayoutComp.js
+++ Examen Final/vending-machine/src/components/VendingMachineLayoutComp/VendingMachineLayoutComp.js
@@ -4,7 +4,7 @@ import { OfferedBeveragesComp } from '../OfferedBeveragesComp/OfferedBeveragesCo
 export const VendingMachineLayoutComp = () => {
   return (
     <div className='parent'>
-      <div className='div1'> <OfferedBeveragesComp/> </div>
+      <div className='div1'> <OfferedBeveragesComp beveragesOnStock={[10,8,10,15]} /> </div>
       <div className='div2'> </div>
       <div className='div3'> </div>
       <div className='div4'> </div>
