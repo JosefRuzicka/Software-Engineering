e123fca159b3baec517ab5601cdc812d6005c2c1
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-23 12:59:23 -0600

    Componente con Grid de bebidas ofrecidas

--- Examen Final/vending-machine/src/App.js
+++ Examen Final/vending-machine/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { OfferedBeveragesComp } from './components/OfferedBeveragesComp/OfferedBeveragesComp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        < OfferedBeveragesComp />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ Examen Final/vending-machine/src/components/OfferedBeveragesComp/OfferedBeveragesComp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './OfferedBeveragesCompStyle.scss';
+
+export const OfferedBeveragesComp = () => {
+  return (
+    <div className='parent'>
+      <div className='div1'> Coca Cola</div>
+      <div className='div2'> Pepsi </div>
+      <div className='div3'> 7-UP </div>
+      <div className='div4'> Canada Dry </div>
+      <div className='div5'> Fanta </div>
+      <div className='div6'> Nestea </div>
+      <div className='div7'> Sprite </div>
+      <div className='div8'> Dr. Pepper</div>
+      <div className='div9'> Mtn. Dew </div>
+    </div>
+  );
+};
