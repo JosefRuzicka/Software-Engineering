5cc88c717aff2a8b6d8cccbc3de97266746789af
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-23 14:26:33 -0600

    typo fix

--- Examen Final/vending-machine/src/components/OfferedBeveragesComp/OfferedBeveragesComp.js
+++ Examen Final/vending-machine/src/components/OfferedBeveragesComp/OfferedBeveragesComp.js
@@ -6,19 +6,19 @@ export const OfferedBeveragesComp = ({beveragesOnStock}) => {
     <div className='parent'>
       <div className='div1'> 
         <p> Coca Cola </p>
-        <p> On stock: {beveragesOnStock[0]} </p>
+        <p> In stock: {beveragesOnStock[0]} </p>
       </div>
       <div className='div2'> 
         <p> Pepsi </p>
-        <p> On stock: {beveragesOnStock[1]} </p>
+        <p> In stock: {beveragesOnStock[1]} </p>
       </div>
       <div className='div3'> 
         <p> Fanta </p>
-        <p> On stock: {beveragesOnStock[2]} </p>
+        <p> In stock: {beveragesOnStock[2]} </p>
       </div>
       <div className='div4'> 
         <p> Sprite </p>
-        <p> On stock: {beveragesOnStock[3]} </p>
+        <p> In stock: {beveragesOnStock[3]} </p>
       </div>
     </div>
   );
