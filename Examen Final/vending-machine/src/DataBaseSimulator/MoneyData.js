3620b8b1d30e934e9ccad757300809ea8968b01a
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-26 21:44:18 -0600

    Adicion de ids y restriccion de casos para unit tests

--- Examen Final/vending-machine/src/DataBaseSimulator/MoneyData.js
+++ Examen Final/vending-machine/src/DataBaseSimulator/MoneyData.js
@@ -1,29 +1,34 @@
 export const MoneyDataBase = [
   {
+    id: 0,
     category: 'coinsWorth25',
     value: 25,
     count: 25,
     depositedInCurrentTransaction: 0
   },
   {
+    id: 1,
     category: 'coinsWorth50',
     value: 50,
     count: 50,
     depositedInCurrentTransaction: 0
   },
   {
+    id: 2,
     category: 'coinsWorth100',
     value: 100,
     count: 30,
     depositedInCurrentTransaction: 0
   },
   {
+    id: 3,
     category: 'coinsWorth500',
     value: 500,
     count: 20,
     depositedInCurrentTransaction: 0
   },
   {
+    id: 4,
     category: 'billsWorth1000',
     value: 1000,
     count: 0,
